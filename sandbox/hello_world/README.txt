681283782fecd72d2162faef2d558efc280596bf
Yehuda Tsimanis <yehuda.tsimanis@gmail.com>
2021-03-29 19:38:59 +0300

    [iverilog] adding basic example

---
+++ sandbox/hello_world/README.txt
@@ -0,0 +1,4 @@
+#Compile
+iverilog -o hello_world hello_world.v
+#Run
+vvp hello_world
---
+++ sandbox/hello_world/hello_world.v
@@ -0,0 +1,7 @@
+module hello_world;
+  initial 
+    begin
+      $display("Hello World!");
+      $finish ;
+    end
+endmodule
